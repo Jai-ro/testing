6b52337712b9db3a2274f5ebd0779f846d8ad946
Jairo <jairojimenezm11@gmail.com>
2024-01-19 04:09:40 -0400

    first commit - test

---
+++ .php_tmp/comedia.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Peliculas</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="Shortcut Icon" type="image/x-icon" href="assets/icons/logoo.png" />
+    <script src="js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="css/sweet-alert.css">
+    <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="js/modernizr.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+
+
+<style>
+
+    .contenedor{
+    position:relative;
+    width:600px;
+    height:390px;
+    padding-left: 20px;
+    padding-top: 30px;
+    background-color:#343437;
+    border-radius: 50px;
+    margin:auto;
+}
+
+
+</style>
+<body>
+    <div class="navbar-lateral full-reset">
+        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+        <div class="full-reset container-menu-movile custom-scroll-containers">
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+            <div class="full-reset" style="background-color:#2B3D51; padding: 10px 0; color:#fff;">
+                <figure>
+                    <img src="assets/img/logoo.png" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
+                </figure>
+                <p class="text-center" style="padding-top: 15px;"><font size="4" face="Algerian">Peliculas</font></p>
+            </div>
+            <div class="full-reset nav-lateral-list-menu">
+                <ul class="list-unstyled">
+                    <li><a href="esperainicio.php"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                    <li>
+                        <div class="dropdown-menu-button"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp; Peliculas <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
+                        <ul class="list-unstyled">
+                        <li><a href="esperaacción.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; La Paz</a></li>
+                            <li><a href="esperacienciaficción.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Oruro  </a></li>
+                            <li><a href="esperacomedia.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Potosí  </a></li>
+                            <li><a href="esperafantasía.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Chuquisaca</a></li>
+                            <li><a href="esperamelodrama.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Cochabamba </a></li>
+                            <li><a href="esperamusical.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Tarija </a></li>
+                            <li><a href="esperaromance.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Santa Cruz</a></li>
+                            <li><a href="esperasuspense.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Beni </a></li>
+                            <li><a href="esperaterror.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Pando  </a></li>
+                        </ul>
+                    </li>
+                    	 <li><a href="espera.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Nuevas Peliculas</a></li>
+
+                             <li><a href="desconectar.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Cerrar</a></li>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-page-container full-reset custom-scroll-containers">
+
+    	 <div class="footer-copyright full-reset all-tittles">Peliculas</div>
+
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles"><center><font size="10" face="Algerian">comedia</font></center></h1>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <ul class="nav nav-tabs nav-justified"  style="font-size: 17px;">
+
+            </ul>
+        </div>
+        <div class="container-fluid">
+
+<div class="container-flat-form">
+<br>
+
+<div class="contenedor">
+
+<div style="position:relative;height:0;padding-bottom:56.23%"><iframe src="https://www.youtube.com/embed/kLM3GLhkjdk" style="position:absolute;width:97%;height:100%;left:0" width="640" height="360" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
+
+
+</div>
+
+<br>
+</div>
+
+        </div>
+        <footer class="footer full-reset">
+
+            <div class="footer-copyright full-reset all-tittles">Peliculas</div>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ .php_tmp/inicio.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Peliculas internacionales</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="Shortcut Icon" type="image/x-icon" href="assets/icons/logoo.png" />
+    <script src="js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="css/sweet-alert.css">
+    <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="js/modernizr.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <div class="navbar-lateral full-reset">
+        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+        <div class="full-reset container-menu-movile custom-scroll-containers">
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+            <div class="full-reset" style="background-color:#2B3D51; padding: 10px 0; color:#fff;">
+                <figure>
+                    <img src="assets/img/logoo.png" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
+                </figure>
+                <p class="text-center" style="padding-top: 15px;"><font size="4" face="Algerian">Peliculas</font></p>
+            </div>
+            <div class="full-reset nav-lateral-list-menu">
+                <ul class="list-unstyled">
+                    <li><a href="esperainicio.php"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                    <li>
+                        <div class="dropdown-menu-button"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp; Peliculas <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
+                        <ul class="list-unstyled">
+                        <li><a href="esperalapaz.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; La Paz</a></li>
+                            <li><a href="esperaoruro.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Oruro  </a></li>
+                            <li><a href="esperapotosi.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Potosí  </a></li>
+                            <li><a href="esperachuquisaca.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Chuquisaca</a></li>
+                            <li><a href="esperacochabamba.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Cochabamba </a></li>
+                            <li><a href="esperatarija.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Tarija </a></li>
+                            <li><a href="esperasantacruz.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Santa Cruz</a></li>
+                            <li><a href="esperabeni.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Beni </a></li>
+                            <li><a href="esperapando.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Pando  </a></li>
+                        </ul>
+                    </li>
+                    	 <li><a href="espera.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Nuevas Peliculas</a></li>
+
+                    	 <li><a href="desconectar.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Cerrar</a></li>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-page-container full-reset custom-scroll-containers">
+
+    	 <div class="footer-copyright full-reset all-tittles">FilmBolivia</div>
+
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles"><center><font size="10" face="Algerian">PELICULAS INTERNACIONALES</font></center></h1>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <ul class="nav nav-tabs nav-justified"  style="font-size: 17px;">
+
+            </ul>
+        </div>
+        <div class="container-fluid"  style="margin: 50px 0;">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <img src="assets/img/cine.jpg" alt="user" class="img-responsive center-box" style="max-width: 110px;">
+                </div>
+                <div class="col-xs-12 col-sm-8 col-md-8 text-justify lead">
+                    <p>"FilmBolivia" nace de la idea simple pero poderosa de acercarte al emocionante mundo del cine boliviano en su máxima expresión. Aunque nuestra página puede carecer de adornos ostentosos, su simplicidad es su mayor fortaleza. Nos centramos en ofrecer una experiencia sin complicaciones para que puedas explorar y disfrutar de lo mejor del cine nacional.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+<div class="container-flat-form">
+<br>
+<br>
+<a href="espera.php">
+<img src="assets/img/alien.png" alt="Biblioteca" class="img-responsive center-box" style="width:70%;">
+</a>
+<br>
+<br>
+</div>
+
+<div class="container-flat-form">
+<br>
+<br>
+<a href="espera.php">
+<img src="assets/img/transformers.png" alt="Biblioteca" class="img-responsive center-box" style="width:70%;">
+</a>
+<br>
+<br>
+</div>
+
+<div class="container-flat-form">
+<br>
+<br>
+<a href="espera.php">
+<img src="assets/img/logan.png" alt="Biblioteca" class="img-responsive center-box" style="width:70%;">
+</a>
+<br>
+<br>
+</div>
+
+<div class="container-flat-form">
+<br>
+<br>
+<a href="espera.php">
+<img src="assets/img/guerrapapas2.png" alt="Biblioteca" class="img-responsive center-box" style="width:70%;">
+</a>
+<br>
+<br>
+</div>
+
+        </div>
+        <footer class="footer full-reset">
+
+            <div class="footer-copyright full-reset all-tittles">FilmBolivia</div>
+        </footer>
+    </div>
+</body>
+</html>
